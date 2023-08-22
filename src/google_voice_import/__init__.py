3cd577610c36afe04db95be2b9c0ce38c9c66207
Kevin J. McNamara <kjmcnamara1@gmail.com>
2023-08-22 12:44:50 -0400

    grab filename metadata from voice

--- src/google_voice_import/__init__.py
+++ src/google_voice_import/__init__.py
@@ -1,17 +1,17 @@
-import pandas as pd
-import pytz
+# import pandas as pd
+# import pytz
 
-"Aug 7, 2023 13:55:11"
+# "Aug 7, 2023 13:55:11"
 
-"Timestamp Now"
-pd.Timestamp.now(tz="US/Central").tz_convert("UTC")
-pd.Timestamp.now()
+# "Timestamp Now"
+# pd.Timestamp.now(tz="US/Central").tz_convert("UTC")
+# pd.Timestamp.now()
 
-print()
-"Message Timestamp"
-pd.Timestamp(1691434511000, unit="ms", tz="UTC")
-pd.Timestamp(1691434511000, unit="ms", tz="UTC").tz_convert("US/Central")
-pd.Timestamp(1691434511000, unit="ms", tz="UTC").tz_convert("US/Central").value / 1e6
+# print()
+# "Message Timestamp"
+# pd.Timestamp(1691434511000, unit="ms", tz="UTC")
+# pd.Timestamp(1691434511000, unit="ms", tz="UTC").tz_convert("US/Central")
+# pd.Timestamp(1691434511000, unit="ms", tz="UTC").tz_convert("US/Central").value / 1e6
 
 
-pytz.all_timezones
+# pytz.all_timezones
